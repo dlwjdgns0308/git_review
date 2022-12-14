e775142dda7b22812286cc9815c7a55d672d9fee
dlwjdgns0308 <dlwjdgns0308@naver.com>
2022-12-14 17:03:47 +0900

    카메라 모듈 지정

--- test.py
+++ test.py
@@ -1,43 +1,13 @@
+import kivy
+kivy.require('1.9.1')
+
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+from kivy.uix.label import Label
+from kivy.uix.camera import Camera
 
-
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
-
+class MainApp(App):
     def build(self):
-        return CameraClick()
+        return Camera(play=True, index=1, resolution=(640,480))
 
-
-TestCamera().run()
+if __name__== "__main__":
+    MainApp().run()
